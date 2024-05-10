0ad4e4ac6826b9b2705e66b36c938b1d6ba4053d
ramszaj513 <jakubr513@gmail.com>
2024-05-10 13:44:20 +0200

    Create model_download_script.py

---
+++ model_download_script.py
@@ -0,0 +1,20 @@
+import dropbox
+
+MODEL_FILE = "pneumonia_detection_modelv2.h5"
+
+# Replace these with your app's credentials
+ACCESS_TOKEN = ''
+
+# Initialize Dropbox client
+dbx = dropbox.Dropbox(ACCESS_TOKEN)
+
+# Specify the path to the file you want to download from Dropbox
+dropbox_file_path = '/path/to/your/file.txt'
+
+# Specify the local path where you want to save the downloaded file
+local_file_path = MODEL_FILE
+
+# Download the file from Dropbox
+with open(local_file_path, "wb") as f:
+    metadata, res = dbx.files_download(dropbox_file_path)
+    f.write(res.content)
