d2a8f05f9d9484cbb5b7f6518a603d95c525e71f
R4ken <57732185+R4ken@users.noreply.github.com>
2024-05-27 21:45:29 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -11,6 +11,7 @@ import torchvision
 from torchvision import transforms
 import preprocess
 
+import pytorch_grad_cam
 from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 from pytorch_grad_cam.utils.image import show_cam_on_image, preprocess_image
