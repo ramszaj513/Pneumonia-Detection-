2c713df9aa4ca8a28763d9aea42406a22e161170
ramszaj513 <jakubr513@gmail.com>
2024-05-27 22:37:19 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -81,7 +81,7 @@ def main():
             plt.axis('off')
             st.pyplot(figure)
     
-            transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])],)
+        transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])],)
         except Exception as e:
             st.error(f"An error occurred: {e}")
 
